379b58208c4deb1acc692dc32c77ec116e5a86f7
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-07-25 16:12:36 +0900

    feat: settable piece to board

--- src/index.vue
+++ src/index.vue
@@ -1,36 +1,62 @@
 <template>
-  <div>
+  <main>
+    <button @click="init()">init</button>
     <ul  v-for="(row, i) of board" :key="i">
-      <li v-for="(column, j) of board[i]" :key="j">
+      <li v-for="(column, j) of board[i]" :key="j" @click="clickedBoard($event, i, j)">
         <div v-if="column === 0" class="blank"></div>
         <div v-else-if="column === 1" class="black"></div>
         <div v-else-if="column === 2" class="white"></div>
       </li>
     </ul>
-  </div>
+    <p v-if="turn === 1">black turn</p>
+    <p v-else>white turn</p>
+  </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+const BLANK = 0
+const BLACK = 1
+const WHITE = 2
+
 export default Vue.extend({
   name: 'App',
   data () {
     return {
-      board: []
+      board: [],
+      turn: 1 // 1: black, 2: white
     }
   },
   created () {
-    for (let i = 0; i < 8; i++) {
-      this.board.push([])
-      for (let j = 0; j < 8; j++) {
-        this.board[i].push(0)
+    this.init()
+  },
+  methods: {
+    init (): void {
+      this.board = []
+      for (let i = 0; i < 8; i++) {
+        this.board.push([])
+        for (let j = 0; j < 8; j++) {
+          this.board[i].push(BLANK)
+        }
+      }
+      this.board[3][3] = BLACK
+      this.board[3][4] = WHITE
+      this.board[4][3] = WHITE
+      this.board[4][4] = BLACK
+    },
+    clickedBoard (event: Event, i: number, j: number): void {
+      if (this.board[i][j] === BLANK) {
+        (<HTMLElement>event.target).classList.remove('blank')
+        if (this.turn === BLACK) {
+          (<HTMLElement>event.target).classList.add('black')
+        } else {
+          (<HTMLElement>event.target).classList.add('white')
+        }
+        this.board[i][j] = this.turn
+        this.turn = this.turn === BLACK ? WHITE : BLACK
       }
     }
-    this.board[3][3] = 1
-    this.board[3][4] = 2
-    this.board[4][3] = 2
-    this.board[4][4] = 1
   }
 })
 </script>
